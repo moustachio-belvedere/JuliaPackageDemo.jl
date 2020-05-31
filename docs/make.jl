e6a6a24fac860a0ad10b06d5bfea5f6b6a03465d
Louis Kaplan <j.kiszle@gmail.com>
2020-05-31 18:30:08 +0100

    neaten up make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,34 +1,49 @@
 using Documenter, Literate, JuliaPackageDemo
 
+"""
+    docprepare()
+
+Convert all source documentation (.jl and .md files)
+into markdown, ready for documentation build, and 
+relevant .jl files into Jupyter notebooks ready for
+deployment to notebooks branch.
+"""
 function docprepare()
     # remove remnants of previous build and recreate staging dir
-    rm("docs/src-staging", force=true, recursive=true)
-    mkdir("docs/src-staging")
+    rm("docs/staging-docs", force=true, recursive=true)
+    mkdir("docs/staging-docs")
 
-    # copy readme to src-staging
-    cp("README.md", "docs/src-staging/index.md")
+    # copy readme to staging-docs
+    cp("README.md", "docs/staging-docs/index.md")
 
     # iterate through src and convert/copy as appropriate
     for file in readdir("docs/src")
         if endswith(file, "md")
-            cp("docs/src/$file", "docs/src-staging/$file")
+            cp("docs/src/$file", "docs/staging-docs/$file")
         end
     end
 
 end
 
-docprepare()
+function maindocbuilder()
+    # prepare doc files from source
+    docprepare()
 
-makedocs(modules=[JuliaPackageDemo],
-         doctest= false, clean=true,
-         format = Documenter.HTML(),
-         sitename="JuliaPackageDemo",
-         source = "src-staging",
-         authors="Louis Kaplan",
-         pages = ["Home" => "index.md",
-                  "API" => "API.md"])
+    # build docs from staging area
+    makedocs(modules=[JuliaPackageDemo],
+            doctest= false, clean=true,
+            format = Documenter.HTML(),
+            sitename="JuliaPackageDemo",
+            source = "staging-docs",
+            authors="Louis Kaplan",
+            pages = ["Home" => "index.md",
+                    "API" => "API.md"])
 
-# deploydocs(repo = "github.com/JuliaRheology/RHEOS.jl.git",
-#            deps = nothing,
-#            make = nothing,
-#            target = "build")
+    # deploydocs(repo = "github.com/JuliaRheology/RHEOS.jl.git",
+    #            deps = nothing,
+    #            make = nothing,
+    #            target = "build")
+
+end
+
+maindocbuilder()
