c47d112fa04c204eb8c50ebdc4b0df1ec4717e7d
Louis Kaplan <j.kiszle@gmail.com>
2020-05-31 15:50:54 +0100

    fill main src file

--- src/JuliaPackageDemo.jl
+++ src/JuliaPackageDemo.jl
@@ -1,5 +1,25 @@
 module JuliaPackageDemo
 
-greet() = print("Hello World!")
+using MittagLeffler
+
+export coolfunction1, coolfunction2
+
+"""
+    coolfunction1(x)
+
+Computes `2*x + x^2` and returns the value.
+"""
+function coolfunction1(x)
+    2*x + x^2
+end
+
+"""
+    coolfunction2(x)
+
+Computes `50*mittleff(0.5, -x) - 10` and returns the value.
+"""
+function coolfunction2(x)
+    50*mittleff(0.5, -x) - 10
+end
 
 end # module
