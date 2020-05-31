0e2f2ce56b501aefcab9b261dee6efa64bd6272c
Louis Kaplan <j.kiszle@gmail.com>
2020-05-31 14:58:30 +0100

    add some dependencies

---
+++ test/runtests.jl
@@ -0,0 +1,14 @@
+using JuliaPackageDemo
+using Test
+
+# include("definitions.jl")
+# include("base.jl")
+# include("datagen.jl")
+# include("IO.jl")
+# include("processing.jl")
+
+# include(joinpath(@__DIR__, "models", "elements.jl"))
+# include(joinpath(@__DIR__, "models", "maxwell.jl"))
+# include(joinpath(@__DIR__, "models", "kelvinvoigt.jl"))
+# include(joinpath(@__DIR__, "models", "zener.jl"))
+# include(joinpath(@__DIR__, "models", "poynting-thomson.jl"))
